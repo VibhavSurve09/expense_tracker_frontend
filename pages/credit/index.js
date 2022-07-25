f3ffda5b779177aa0925b258f322f51a3e9fd71f
Vibhav0 <vibhav.surve@xaviers.edu.in>
2022-07-25 23:41:06 +0530

    Working on credit

--- pages/credit/index.js
+++ pages/credit/index.js
@@ -1,12 +1,25 @@
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { Container } from '@mui/system';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
-
 function Credit() {
+  const [credit, setCredit] = useState([]);
+  useEffect(() => {
+    const getCredit = async () => {
+      const response = await axios.get(`${process.env.API}/credit`, {
+        withCredentials: true,
+      });
+      const credit = await response.data.json();
+      setCredit(credit);
+    };
+    getCredit();
+  }, []);
+  console.log(credit);
   return (
     <Box>
       <Navbar />
+
       <Container>This page shall contain all about credit</Container>
     </Box>
   );
